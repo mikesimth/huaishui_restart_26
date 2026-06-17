5e302fe8643c23c2732c9c6e625d404d9cec4d8a
李峄 <liyi@bestpay.com.cn>
2026-06-17 14:23:52 +0800

    初始化项目、安装配置husky

---
+++ my-app/app/page.tsx
@@ -0,0 +1,12 @@
+import { ArrowUpIcon } from "lucide-react"
+
+import { Button } from "@/components/ui/button"
+
+export default function ButtonDemo() {
+  return (
+    <div className="flex flex-wrap items-center gap-2 md:flex-row">
+      <Button variant="outline">Button</Button>
+     
+    </div>
+  )
+}
